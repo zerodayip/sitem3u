7f4f8af5b2b4385322d4c7ccc4e46f2c142336a6
MDuymazz <duymaz.mrt@gmail.com>
2025-04-22 00:13:37 +0700

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,19 +1,22 @@
+import chromedriver_binary_auto
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
+# Chrome seçeneklerini ayarlayın
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")  # Tarayıcıyı arka planda çalıştırmak için
 
-# ChromeDriver'ı otomatik olarak indirmek ve çalıştırmak
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+# ChromeDriver servisini başlatın
+service = Service()
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
-url = 'https://daddylive.mp/schedule/schedule.html'
-driver.get(url)
+# Web sayfasını açın
+driver.get("https://daddylive.mp/schedule/schedule.html")
 
-# Sayfanın HTML içeriğini almak
+# Sayfa kaynağını alın
 html_content = driver.page_source
 print(html_content)
 
+# Tarayıcıyı kapatın
 driver.quit()
