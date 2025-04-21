fe2f6944f123868fa2fa09d505d6f08cfd21d88d
MDuymazz <duymaz.mrt@gmail.com>
2025-04-21 22:58:12 +0700

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,9 +1,1 @@
-import requests
 
-url = "https://daddylive.mp/schedule/schedule-generated.json"  # Dilersen diğer domainleri de deneyebilirsin
-try:
-    response = requests.get(url, timeout=10)
-    data = response.json()
-    print(data)
-except Exception as e:
-    print("Hata oluştu:", e)
