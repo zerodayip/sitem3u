f5e4d74c3c0fe840580b98a2831462a2c1c12bb6
MDuymazz <duymaz.mrt@gmail.com>
2025-04-22 00:08:22 +0700

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,30 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-import time
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Tarayıcıyı arka planda çalıştırır
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
 
-# WebDriver'ı başlatıyoruz
-service = Service('/usr/local/bin/chromedriver')
-driver = webdriver.Chrome(service=service, options=chrome_options)
+# ChromeDriver'ı otomatik olarak indirmek ve çalıştırmak
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-# Hedef URL'yi açıyoruz
 url = 'https://daddylive.mp/schedule/schedule.html'
 driver.get(url)
 
-# Sayfanın dinamik olarak yüklenmesi için beklemek
-time.sleep(5)
-
-# HTML içeriğini alıyoruz
+# Sayfanın HTML içeriğini almak
 html_content = driver.page_source
-
-# Sayfanın HTML içeriğini yazdırıyoruz
 print(html_content)
 
-# WebDriver'ı kapatıyoruz
 driver.quit()
