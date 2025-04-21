9591208d9588bcecb62c6e4c3e7a05be8c5ad222
MDuymazz <duymaz.mrt@gmail.com>
2025-04-22 00:16:47 +0700

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -7,9 +8,10 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-driver.get("https://daddylive.mp/schedule/schedule.html")
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
 
+driver.get("https://daddylive.mp/schedule/schedule.html")
 html = driver.page_source
 print(html)
 
