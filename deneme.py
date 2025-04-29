c5bc62ee2a7a42b380168ab412f4066ef432f931
zerodayip <duymaz.mrt@gmail.com>
2025-04-29 13:32:55 +0700

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -10,14 +10,22 @@ with sync_playwright() as p:
         if ".m3u8" in request.url:
             m3u8_links.append(request.url)
 
+    # Ağ trafiğini dinlemeye başla
     page.on("request", handle_request)
 
-    # Örnek kanal sayfası
-    print("Sayfa yükleniyor...")
+    # Sayfayı aç
     page.goto("https://canlitv.com/trt1-canli")
-    page.wait_for_timeout(5000)
 
-    # Bağlantılar
+    # Iframe'i seç
+    iframe = page.frame(name="Player")  # Burada iframe'in 'name' veya 'id' özelliklerine göre seçiyoruz
+
+    # Eğer iframe'deki video öğesini bulabilirsen, buradaki oynatma işlemini tetikleyebilirsin:
+    # iframe.click('button#play-button')  # Eğer bir play butonu varsa bunu tetikle
+
+    # Yavaşça bekleyerek tüm medya kaynaklarının yüklenmesini sağla
+    page.wait_for_timeout(10000)  # Sayfa yüklendikten sonra 10 saniye bekle
+
+    # Bulunan m3u8 bağlantılarını yazdır
     if m3u8_links:
         print("Bulunan m3u8 bağlantıları:")
         for url in set(m3u8_links):
