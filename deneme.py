d92793292c9420a5c2a302eb97cdbe08db37a350
zerodayip <duymaz.mrt@gmail.com>
2025-04-29 17:21:17 +0700

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,13 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
-# Sayfa URL'sini belirt
 url = "https://canlitv.com/?sayfa=1"
+base_url = "https://canlitv.com"
 
-# Sayfanın HTML içeriğini almak
 response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
 
-# Sayfa başarılı bir şekilde alındıysa HTML içeriğini yazdır
-if response.status_code == 200:
-    print(response.text)
-else:
-    print("Sayfa alınamadı, HTTP Durum Kodu:", response.status_code)
+kanallar = soup.select("li.tv.fk_")
+
+for kanal in kanallar:
+    a_tag = kanal.find("a")
+    if a_tag:
+        kanal_adi = a_tag.text.strip()
+        detay_url = base_url + a_tag.get("href")
+        print(f"{kanal_adi} - {detay_url}")
